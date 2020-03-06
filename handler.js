a094f167827e363804a4fae0f706545dc9284902
Ethan Jones <joneetha@oregonstate.edu>
2020-03-05 21:51:59 -0800

    enable posting playlist

--- handler.js
+++ handler.js
@@ -4,53 +4,73 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.saveUser = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.saveUser = async (event, context, callback) => {
+  let params = {
+    TableName: 'users',
+    Item: {
+      'PK': event.id,
+      'SK': event.email
+    }
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  await ddb.put(params, error => {
+    if (error) {
+      console.log(error)
+      callback(null, {
+        statusCode: 400,
+        body: "Failed"
+      });
+    }
+  });
+  callback(null, {
+    statusCode: 200,
+    body: "Ok"
+  });
 };
 
 module.exports.getPlaylist = async event => {
-  return {
+  await ddb.scan({
+    TableName: 'users'
+  }, error => {
+    if (error) {
+      callback(null, {
+        statusCode: 400,
+        body: "Failed"
+      });
+    }
+  });
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    body: "Ok"
+  });
 };
 
-module.exports.postPlaylist = async event => {
-  return {
+module.exports.postPlaylist = async (event, context, callback) => {
+  if (!event.userId) {
+    callback(null, {
+      statusCode: 400,
+      body: "No user id supplied"
+    });
+  }
+  for (let songId of event.songs) {
+    let params = {
+      TableName: 'users',
+      Item: {
+        'PK': `playlist_${event.userId}`,
+        'SK': songId
+      }
+    };
+    await ddb.put(params, error => {
+      if (error) {
+        console.log(error);
+        callback(null, {
+          statusCode: 400,
+          body: "Failed"
+        });
+      }
+    });
+  }
+  callback(null, {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    body: "Ok"
+  });
 };
