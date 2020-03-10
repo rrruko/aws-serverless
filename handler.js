027a24b2489608af9775ec911369ce5cf80365d5
Ethan Jones <joneetha@oregonstate.edu>
2020-03-09 19:09:16 -0700

    successfully push to queue when invoked locally

--- handler.js
+++ handler.js
@@ -81,3 +81,44 @@ module.exports.postPlaylist = async (event, context, callback) => {
     body: "Ok"
   });
 };
+
+module.exports.play = async (event, context, callback) => {
+  let artist = event.artist;
+  let album = event.album;
+  let song = event.song;
+  if (!artist || !album || !song) {
+    callback(null, {
+      statusCode: 400,
+      body: "Missing parameters"
+    });
+  }
+  let sqs = new AWS.SQS({});
+  await sqs.sendMessage({
+    QueueUrl: 'https://sqs.us-east-1.amazonaws.com/195191189173/cs493-pubsub-2020-03-09.fifo',
+    MessageAttributes: {
+      "artist": { DataType: "String", StringValue: artist },
+      "album": { DataType: "String", StringValue: album },
+      "song": { DataType: "String", StringValue: song }
+    },
+    MessageBody: "owo",
+    MessageDeduplicationId: Date.now().toString(),
+    MessageGroupId: "Group"
+  }, (error, data) => {
+    if (data) {
+      callback(null, {
+        statusCode: 200,
+        body: `Ok (${data})`
+      });
+    } else if (error) {
+      callback(null, {
+        statusCode: 400,
+        body: `Error (${error})`
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: `No response from sqs`
+      });
+    }
+  }).promise();
+};
